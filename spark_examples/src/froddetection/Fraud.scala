35ff6047e5c5e21b5767bcb36f6ff42bfa3ae8db
Dharmik Kachhia <dharmik.kachhia@yahoo.com>
2019-04-30 16:17:18 +0530

    Spark functions demos for Frod detection
    Refactored Main function and moved the function demos to different class

--- spark_examples/src/froddetection/Fraud.scala
+++ spark_examples/src/froddetection/Fraud.scala
@@ -27,65 +27,57 @@ object Fraud {
       .json("inputs/finances-small.json").cache
 
     import spark.implicits._
-    
-    
-    val windowSpecPrevious4account = Window.partitionBy($"AccountNumber").orderBy($"Date").rowsBetween(-4, 0)
+
+    //start with previous 4 rows and end with current rows
+    val windowSpecPrevious4account = Window.partitionBy($"AccountNumber").orderBy($"Date").rowsBetween(-4, 0) 
     val rollingAvgPrevious4account = avg($"Amount").over(windowSpecPrevious4account).as("Last4Avg")
-    
+
     val financeDf = financeDfRaw
       .na.drop("all", Seq("ID", "Account", "Amount", "Date", "Description")) //it will drop records with _corrupt_record column
       .na.fill("Unknown", Seq("Description")) //if description is null, fill with unknown value
       .where($"Amount" =!= 0 || $"Description" === "Unknown")
-      .selectExpr("Account.Number as AccountNumber", "Amount", "Description","to_date(`Date`, 'MM/dd/yyyy') as Date") //extracting columns from nested Account Struc
+      .selectExpr("Account.Number as AccountNumber", "Amount", "Description", "to_date(`Date`, 'MM/dd/yyyy') as Date") //extracting columns from nested Account Struc
       .select($"*", rollingAvgPrevious4account)
-      
+
     //financeDf.write.mode(SaveMode.Overwrite).parquet("D:\\upload\\spark_data\\crap\\frod\\clean")
+    financeDf.write.mode(SaveMode.Overwrite).csv("D:\\upload\\spark_data\\crap\\frod\\clean")
     financeDf.show(false)
-    
-      
+
     println("********************** End of Fraud detection spark job **********************")
   }
-  
-  def getAccountStatistics(financeDf:DataFrame,spark:SparkSession):DataFrame = {
+
+  def getAccountStatistics(financeDf: DataFrame, spark: SparkSession): DataFrame = {
     println("Printing account summary")
     import spark.implicits._
+
     val accountSummary = financeDf
-      .select($"AccountNumber", $"Amount", $"Description", to_date($"Date","MM/dd/yyyy") )
+      .select($"AccountNumber", $"Amount", $"Description", to_date($"Date", "MM/dd/yyyy"))
       .groupBy($"AccountNumber")
-      .agg( avg("Amount").as("AverageTransaction"), 
-            sum("Amount").as("TotalTransaction"),
-            count("Amount").as("NumberOfTransaction"),
-            max("Amount").as("MaxTransaction"),
-            collect_set($"Description").as("UniqueDescription") )
+      .agg(avg("Amount").as("AverageTransaction"),
+        sum("Amount").as("TotalTransaction"),
+        count("Amount").as("NumberOfTransaction"),
+        max("Amount").as("MaxTransaction"),
+        collect_set($"Description").as("UniqueDescription"))
       .coalesce(5) //to reduce number of task from default 200 to 5s
     accountSummary.show(false)
     //we can use explode function to get back all the description mapped to that account number so 
-    //explode is inverse of group by clause ---- IMP
+    //EXPLODE is inverse of group by clause ---- IMP
     accountSummary
   }
-  
-  def getAccountDistinctAccountNumberAndFullName(financeDfRaw:DataFrame,spark:SparkSession):DataFrame = {
-    import spark.implicits._
-    println("Showing only name and account numbers")
-    val accInfoDf = financeDfRaw.
-      select( concat($"Account.FirstName", lit(" "), $"Account.LastName").as("FullName"), $"Account.Number".as("AccountNumber") ).distinct
-    accInfoDf
-  }
-  
-  def handleCorruptRecords(df:DataFrame,spark:SparkSession) = {
+
+  def handleCorruptRecords(df: DataFrame, spark: SparkSession) = {
     //checking if the _corrup_records column exists or not
     import spark.implicits._
     if (df.hasColumn("_corrupt_record")) { //using implicit class functions
       println("Extracting corrupt records and saving into other location")
       df.filter($"_corrupt_record".isNotNull).select("_corrupt_record")
-                  .write.mode(SaveMode.Overwrite).option("header", true).csv("D:\\upload\\spark_data\\crap\\frod\\corrupt")
+        .write.mode(SaveMode.Overwrite).option("header", true).csv("D:\\upload\\spark_data\\crap\\frod\\corrupt")
     }
-    
+
     implicit class DataFrameHelper(df: DataFrame) {
       import scala.util.Try //to avoid AnalysisException
       def hasColumn(column: String) = Try(df(column)).isSuccess
     }
   }
 
-  
 }
