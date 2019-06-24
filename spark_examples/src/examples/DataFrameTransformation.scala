554c177857366577b8fba4123dabe713bd880c04
Dharmik Kachhia <dharmik.kachhia@yahoo.com>
2019-06-24 15:26:20 +0530

    Transformation dataframe updates
    1 - Added dataframe cast operation
    2 - Exception handling

--- spark_examples/src/examples/DataFrameTransformation.scala
+++ spark_examples/src/examples/DataFrameTransformation.scala
@@ -4,7 +4,6 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SaveMode
 import java.nio.charset.StandardCharsets
-import org.apache.spark.ui.SparkUI
 import util.SparkUtil._
 import org.apache.spark.sql.Row
 import org.apache.spark.TaskContext
@@ -15,9 +14,14 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 import scala.collection.mutable.ListBuffer
+import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.types.LongType
+import scala.util.Success
+import scala.util.Failure
 
 object DataFrameTransformation {
-  
+
+  //case class Mapping(DESTINATION_COL_NAME: String, DESTINATION_DATATYPE: String,SOURCE_COL_NAME: String, SOURCE_DATATYPE: String,TRANSFORM_TYPE: String, SOURCE_EXPRESSION: String)
 
   def main(args: Array[String]): Unit = {
     println("Start of DataFrameTransformation demo example")
@@ -33,6 +37,7 @@ object DataFrameTransformation {
 
     val destDF = transformDF(srcDF, mappings, spark)
     destDF.show
+    destDF.printSchema
 
     println("End of DataFrameTransformation demo example")
   }
@@ -51,14 +56,24 @@ object DataFrameTransformation {
           case "FUNCTION" => tempDF = performFunctionTransformation(mapping, tempDF, spark)
           case _          => tempDF
         }
-
+        tempDF = castDestinationToDataType(tempDF, mapping, spark)
     }
+
     tempDF = tempDF.drop(columnsToDrop: _*)
     tempDF
   }
 
   def columnValueMapping(mapping: Mapping, srcDF: DataFrame, spark: SparkSession): DataFrame = {
-    srcDF.withColumn(mapping.DESTINATION_COL_NAME, col(mapping.SOURCE_COL_NAME))
+    val source_column = mapping.SOURCE_COL_NAME
+    val tempDF = try {
+      srcDF.withColumn(mapping.DESTINATION_COL_NAME, col(source_column))
+    } catch {
+      case unknown: Exception => {
+        println(s"Column not present in source : $source_column")
+        srcDF
+      }
+    }
+    tempDF
   }
 
   def getTransformationMapping(spark: SparkSession): DataFrame = {
@@ -66,13 +81,35 @@ object DataFrameTransformation {
   }
 
   def getSourcePatient = {
-    List(PPat("AAA01", "Joe", "Root", "Birmingham", "CX0089", "M", "Wales"),
-      PPat("AAA02", "Jason", "Holder", "Cape Guena", "XX0000", "M", "Test State"),
-      PPat("AAA03", "Virat", "Kohli", "Delhi", "110000", "M", "Delhi"))
+    List(PPat("10001", "Joe", "Root", "Birmingham", "00089", "M", "Wales"),
+      PPat("10002", "Jason", "Holder", "Cape Guena", "100000", "M", "Test State"),
+      PPat("10003", "Virat", "Kohli", "Delhi", "110000", "M", "Delhi"))
   }
 
   val performFunctionTransformation = (mapping: Mapping, srcDF: DataFrame, spark: SparkSession) =>
     srcDF.withColumn(mapping.DESTINATION_COL_NAME, expr(mapping.SOURCE_EXPRESSION))
 
-
+  def castDestinationToDataType(transformedDF: DataFrame, mapping : Mapping, Spark: SparkSession): DataFrame = {
+      import scala.util.Try
+      /**
+       * to handle cases
+       * when cast of column does not work and gives null values but not an exception
+       * handle actual exception when parsing Dates and timestamp
+       */
+      if (transformedDF.hasColumn(mapping.DESTINATION_COL_NAME))
+        transformedDF.withColumn(mapping.DESTINATION_COL_NAME, col(mapping.DESTINATION_COL_NAME).cast(mapping.DESTINATION_DATATYPE))
+      else transformedDF
+  }
+    
+  def getSparkDataTypeFromDBDataType(dbDataType:String) : DataType = {
+    dbDataType match {
+      case "Int" => IntegerType
+      case "Long" => LongType
+    }
+  }
+  
+  implicit class DataFrameHelper(df: DataFrame) {
+      import scala.util.Try //to avoid AnalysisException
+      def hasColumn(column: String) = Try(df(column)).isSuccess
+}
 }
